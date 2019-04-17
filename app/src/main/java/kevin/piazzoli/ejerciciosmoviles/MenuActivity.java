c43da1bbea8ada246e8c7128686b5432c40ee2ab
kevin4dhd <kevin3dhd@gmail.com>
2019-04-17 15:08:38 -0500

    Primer Commit

--- app/src/main/java/kevin/piazzoli/ejerciciosmoviles/MenuActivity.java
+++ app/src/main/java/kevin/piazzoli/ejerciciosmoviles/MenuActivity.java
@@ -2,12 +2,26 @@ package kevin.piazzoli.ejerciciosmoviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button btnPrimerEjercicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnPrimerEjercicio = findViewById(R.id.btnPrimerEjercicio);
+
+        btnPrimerEjercicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
